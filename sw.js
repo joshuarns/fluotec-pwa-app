96d603faec2b6443d6f98030a7fc4f2b7ba7345f
lnx1337 <joshua13375@gmail.com>
2019-03-11 21:43:13 -0600

    Actualización de archivos y codigo

--- sw.js
+++ sw.js
@@ -16,49 +16,55 @@ const CACHE_NAME = 'v1_cache_programador_fitness',
 
 //durante la fase de instalación, generalmente se almacena en caché los activos estáticos
 self.addEventListener('install', e => {
+  console.log('Evento: Service Worker instalado')
   e.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache)
-          .then(() => self.skipWaiting())
-      })
-      .catch(err => console.log('Falló registro de cache', err))
+    .then(cache => {
+      console.log('Archivos en cache')
+      return cache.addAll(urlsToCache)
+        .then(() => self.skipWaiting())
+    })
+    .catch(err => console.log('Falló registro de cache', err))
   )
 })
 
 //una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
 self.addEventListener('activate', e => {
+  console.log('Evento: Service Worker Activado')
   const cacheWhitelist = [CACHE_NAME]
-
   e.waitUntil(
     caches.keys()
-      .then(cacheNames => {
-        return Promise.all(
-          cacheNames.map(cacheName => {
-            //Eliminamos lo que ya no se necesita en cache
-            if (cacheWhitelist.indexOf(cacheName) === -1) {
-              return caches.delete(cacheName)
-            }
-          })
-        )
-      })
-      // Le indica al SW activar el cache actual
-      .then(() => self.clients.claim())
+    .then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          //Eliminamos lo que ya no se necesita en cache
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName)
+          }
+        })
+      )
+    })
+    // Le indica al SW activar el cache actual
+    .then(() => {
+      console.log('El cache esta limpio y se ha actualizado')
+      return self.clients.claim()
+    })
   )
 })
 
 //cuando el navegador recupera una url
 self.addEventListener('fetch', e => {
+  console.log('Service Worker Recuperado')
   //Responder ya sea con el objeto en caché o continuar y buscar la url real
   e.respondWith(
     caches.match(e.request)
-      .then(res => {
-        if (res) {
-          //recuperar del cache
-          return res
-        }
-        //recuperar de la petición a la url
-        return fetch(e.request)
-      })
+    .then(res => {
+      console.log('Recuperando cache')
+      if (res) {
+        return res
+      }
+      //recuperar de la petición a la url
+      return fetch(e.request)
+    })
   )
 })
